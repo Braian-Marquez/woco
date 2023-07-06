fc45fae16aeae94987a9d4d65c7477231a81583f
braian95 <braianmarq95@gmail.com>
2023-07-06 15:01:31 -0300

    refactor cors filter

--- woco/src/main/java/com/nocountry/woco/WocoApplication.java
+++ woco/src/main/java/com/nocountry/woco/WocoApplication.java
@@ -3,11 +3,13 @@ package com.nocountry.woco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @SpringBootApplication
+@ComponentScan(basePackages = "com.nocountry.woco.config")
 public class WocoApplication {
 
 	public static void main(String[] args) {
