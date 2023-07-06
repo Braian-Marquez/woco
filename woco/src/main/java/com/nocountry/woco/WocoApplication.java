c7730d2f8211f256443c487a9e8165753a931499
braian95 <braianmarq95@gmail.com>
2023-07-06 14:06:36 -0300

    refactor cors

--- woco/src/main/java/com/nocountry/woco/WocoApplication.java
+++ woco/src/main/java/com/nocountry/woco/WocoApplication.java
@@ -2,6 +2,9 @@ package com.nocountry.woco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WocoApplication {
@@ -9,5 +12,16 @@ public class WocoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WocoApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer getConfiguration() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
--- woco/src/main/java/com/nocountry/woco/auth/controller/UserAuthController.java
+++ woco/src/main/java/com/nocountry/woco/auth/controller/UserAuthController.java
@@ -17,7 +17,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
-
+@CrossOrigin(origins = "*", methods= {GET, POST, PUT, DELETE})
 @RestController
 @RequestMapping("/auth")
 public class UserAuthController {
