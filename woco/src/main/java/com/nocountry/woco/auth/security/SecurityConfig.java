c57335479c05f648fd81577451498e8344129ef8
braian95 <braianmarq95@gmail.com>
2023-07-06 21:52:14 -0300

    refactor cors

--- woco/src/main/java/com/nocountry/woco/auth/security/SecurityConfig.java
+++ woco/src/main/java/com/nocountry/woco/auth/security/SecurityConfig.java
@@ -76,7 +76,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
+        http.cors().configurationSource(request -> {
+                    CorsConfiguration cors = new CorsConfiguration();
+                    cors.setAllowedOrigins(Arrays.asList("*"));
+                    cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    cors.setAllowedHeaders(Arrays.asList("*"));
+                    cors.setExposedHeaders(Arrays.asList("*"));
+                    return cors;
+                })
+                .and()
                 .csrf()
                 .disable()
                 .sessionManagement()
