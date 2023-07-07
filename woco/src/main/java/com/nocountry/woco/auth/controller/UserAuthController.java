1ca63101a907d7a76cb1a1b74a2eae2d31c4a72d
braian95 <braianmarq95@gmail.com>
2023-07-07 17:40:48 -0300

    refactor cors filter nuevo

--- woco/src/main/java/com/nocountry/woco/auth/controller/UserAuthController.java
+++ woco/src/main/java/com/nocountry/woco/auth/controller/UserAuthController.java
@@ -16,8 +16,7 @@ import javax.validation.Valid;
 
 import java.io.IOException;
 
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-@CrossOrigin(origins = "*", methods= {GET, POST, PUT, DELETE})
+@CrossOrigin(origins="**")
 @RestController
 @RequestMapping("/auth")
 public class UserAuthController {
