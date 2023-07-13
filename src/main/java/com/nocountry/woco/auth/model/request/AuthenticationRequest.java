a246cc85890bcd6f51d1b6f63d9b1ea838cf4432
Erik Jose <64227190+Erik1935@users.noreply.github.com>
2023-07-13 09:08:06 -0600

    Add files via upload

--- src/main/java/com/nocountry/woco/auth/model/request/AuthenticationRequest.java
+++ src/main/java/com/nocountry/woco/auth/model/request/AuthenticationRequest.java
@@ -1,11 +1,10 @@
 package com.nocountry.woco.auth.model.request;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Size;
 
 
 @Getter
--- src/main/java/com/nocountry/woco/model/entity/Cowork.java
+++ src/main/java/com/nocountry/woco/model/entity/Cowork.java
@@ -2,9 +2,8 @@ package com.nocountry.woco.model.entity;
 
 import java.util.List;
 
+import jakarta.persistence.*;
 import lombok.*;
-
-import javax.persistence.*;
 
 @Entity
 @Data
